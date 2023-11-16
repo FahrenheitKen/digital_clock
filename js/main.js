98c26f5196b290049543956e234b9e142d966f58
fahrenheitken <imanikkenzo@gmail.com>
2023-11-17 02:05:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const WEEK = ["Sun", "Mon", "Teu", "Wed", "Thu", "Fri", "Sat"];
+
+function updatetime (){
+    var now= new Date ();
+
+    document .getElementById("time").innerText=
+        zeroPadding(now.getHours(), 2) + ":" +
+        zeroPadding(now.getMinutes(), 2) + ":" +
+        zeroPadding(now.getSeconds(), 2);
+    document .getElementById("date").innerText=
+        now.getFullYear() + "-" +
+        zeroPadding(now.getMonth() + 1, 2) + "-" +
+        zeroPadding(now.getDate(), 2) + " " +
+        WEEK[now.getDay()];
+}
+updatetime();
+setInterval(updatetime, 1000);
+
+function zeroPadding (num, digit) {
+    return String(num).padStart(digit, "0")
+}
